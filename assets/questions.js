95e501de019430042b87ac2b22f8950c8bc728dc
Luke Robinett <lukebootcamp@gmail.com>
2019-11-17 13:57:12 -0800

    Added additional questions to questions.js

--- assets/questions.js
+++ assets/questions.js
@@ -9,6 +9,21 @@ var questions = [
   title: "The condition in an if / else statement is enclosed within ____.",
   choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
   answer: "parentheses"
+ },
+ {
+  title: "Which property returns the length of an array?",
+  choices: ["textContent", "find", "length", "count"],
+  answer: "length"
+ },
+ {
+  title: "A variable declared inside a function can be accessed outside that function",
+  choices: ["True", "False"],
+  answer: "False"
+ },
+ {
+  title: "The JavaScript command console.log(\"10\" + 5) will output which of the following?",
+  choices: ["105", "10 + 5", "15", "undefined"],
+  answer: "105"
  }
 ];
 
